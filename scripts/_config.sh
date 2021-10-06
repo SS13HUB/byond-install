f5778938a62d6f8394f291971124d29a69be595a
Gesugao-san <gesugao.san@gmail.com>
2021-10-06 18:10:23 +0300

    In-line list

--- scripts/_config.sh
+++ scripts/_config.sh
@@ -9,7 +9,7 @@
 # https://stackoverflow.com/q/5725296
 
 BYONDUrl="http://www.byond.com/download/build/"
-saveFile="./tmp.txt"
+#saveFile="./tmp.txt"
 
 # magenta="\033[35m"
 # green="\033[32m"
@@ -28,26 +28,56 @@ get_BYOND_version_hardcoded() {
 
 get_BYOND_versions_list() {
 	# TODO: ping to http://www.byond.com/
-	SAVE_TO_FILE=$1
+	#SAVE_TO_FILE=$1
 	# https://sed.js.org/index.html
 	versions=$(echo "cat //html/body/pre/a" |
 	xmllint --html --shell $BYONDUrl |
 	sed '/^\/\s>/d' |
 	sed '/^\s-------/d' |
 	sed '/"[a-z?/]/d' |
-	sed 's/^[^"]*"\([^"]*\)\/".*/\1/')
+	sed 's/^[^"]*"\([^"]*\)\/".*/\1/' |
+	# https://stackoverflow.com/a/1252191
+	sed ':a;N;$!ba;s/\n/, /g')
 	# 354
 	# 355
 	# ...
 	# 514
 	# 515
 	# LATEST
-	echo "versions: $versions"
-	echo "Versions in file \"$SAVE_TO_FILE\"."
+	#versions=$(sed ':a;N;$!ba;s/\n/ /g' "$versions")
+	echo "All versions:"
+	echo "$versions"
+	# https://unix.stackexchange.com/a/444864
+	echo "List versions:"
+	# counter=0
+	# tmp=""
+	# for i in ${versions//,/ }
+	# do
+	# 	counter=$(( counter + 1 ))
+	# 	if [ "$counter" -gt 3 ]; then
+	# 		counter=0
+	# 		tmp=$("$i"+"$tmp")
+	# 	else
+	# 		echo "$tmp"
+	# 	fi
+	# done
+	COUNT=0
+	RESULT=""
+	for X in $versions
+	do
+		if [ "${COUNT}" = 2 ]
+		then
+			RESULT="${X}"
+			COUNT=0
+		else
+			echo "${RESULT}, ${X}"
+			RESULT=""
+		fi
+		COUNT=$((COUNT + 1))
+	done
 }
 
 #
 
 get_BYOND_version_hardcoded
-get_BYOND_versions_list $saveFile
-
+get_BYOND_versions_list
