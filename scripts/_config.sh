20df4770bbec35648c0be25add528919f59ce418
Gesugao-san <gesugao.san@gmail.com>
2021-10-06 11:13:02 +0300

    I spent all day on this line

--- scripts/_config.sh
+++ scripts/_config.sh
@@ -10,3 +10,5 @@ unset imported;
 
 echo "BYOND_MAJOR_HARDCODED = $BYOND_MAJOR_HARDCODED"
 echo "BYOND_MINOR_HARDCODED = $BYOND_MINOR_HARDCODED"
+
+echo "cat //html/body/pre/a" | xmllint --html --shell http://www.byond.com/download/build/ | sed '/^\/\s>/d' | sed '/^\s-------/d' | sed '/[0-9]/!d'
