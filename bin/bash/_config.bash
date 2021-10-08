8ad915fe0a1e949ba2f335c3b31ea0733196236a
Gesugao-san <gesugao.san@gmail.com>
2021-10-08 22:00:00 +0300

    Version separated and Py3 version added

--- bin/bash/_config.bash
+++ bin/bash/_config.bash
@@ -17,9 +17,9 @@ BYONDUrl="http://www.byond.com/download/build/"
 
 # BYOND version
 get_BYOND_version_hardcoded() {
-	imported=$(awk '/^BYOND_MAJOR_HARDCODED/{print $3}' _config.ini)
+	imported=$(awk '/^BYOND_MAJOR_HARDCODED/{print $3}' '_config.ini') # -F " = "
 	export BYOND_MAJOR_HARDCODED=$imported;
-	imported=$(awk '/^BYOND_MINOR_HARDCODED/{print $3}' _config.ini)
+	imported=$(awk '/^BYOND_MINOR_HARDCODED/{print $3}' '_config.ini')
 	export BYOND_MINOR_HARDCODED=$imported;
 	unset imported;
 	echo "BYOND_MAJOR_HARDCODED = $BYOND_MAJOR_HARDCODED"
---
+++ bin/python/_config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Authors: @Gesugao-san;
+# Licence: MIT
+
+# Usefull links:
+# https://www.google.com/search?q=python+parsing+html
+# https://stackoverflow.com/a/419185
+
+
+def say_hello_world():
+    print ("Hello world.")
+
+# Python program to use
+# main for function call.
+if __name__ == "__main__":
+	say_hello_world()
